acca8df5e8310833afffdbec29e5c8f6b9003150
BT_1N3_15 <wchen1325@bths.edu>
2019-04-01 08:33:08 -0400

    getproblem and nextproblem methods functioning

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,22 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int firstInt;
+    private int secondInt;
+
+    public MultPractice(int first, int second)
+    {
+        this.firstInt = first;
+        this.secondInt = second;
+    }
+
+    public String getProblem() {
+        String problem = firstInt + " Times " + secondInt;
+        return problem;
+    }
+
+    public void nextProblem() {
+        secondInt++;
+    }
 }
